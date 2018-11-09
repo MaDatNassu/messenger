dfa8d87bcb64f0918d5353db18ed454d3d2c3559
MaDatNassu <madatman972@gmail.com>
2018-11-09 01:50:17 +0100

    Initial Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="fr">
+    @include('partials.head')
+    <body>
+      @yield('content')
+    </body>
+    @include('partials.foot')
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+  <ul class='test'>
+    <li>test</li>
+  </ul>
+@endsection

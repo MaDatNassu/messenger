fedc3d320e374f000a4aa196085d5118bdcfbb3d
MaDatNassu <madatman972@gmail.com>
2018-11-11 00:06:36 +0100

    update homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,18 +4,45 @@
   <div class="threads">
     <ul class="unstyled-list">
       <li class="thread">
-        Cras justo odio
-        <span class="pin">14</span>
+        <figure class="contact-img">
+          <img src="https://dummyimage.com/50x50/00aeff/000" alt="Contact">
+        </figure>
+        <div class="middle-side">
+          <p class="contact-name">Lorem ipsum</p>
+          <p class="last-message">Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div class="right-side">
+          <span class="pin">14</span>
+          <p class="last-message-date">00:14</p>
+        </div>
       </li>
 
       <li class="thread">
-        Dapibus ac facilisis in
-        <span class="pin">2</span>
+        <figure class="contact-img">
+          <img src="https://dummyimage.com/50x50/00aeff/000" alt="Contact">
+        </figure>
+        <div class="middle-side">
+          <p class="contact-name">Lorem ipsum</p>
+          <p class="last-message">Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div class="right-side">
+          <span class="pin">2</span>
+          <p class="last-message-date">22:57</p>
+        </div>
       </li>
 
       <li class="thread">
-        Morbi leo risus
-        <span class="pin">1</span>
+        <figure class="contact-img">
+          <img src="https://dummyimage.com/50x50/00aeff/000" alt="Contact">
+        </figure>
+        <div class="middle-side">
+          <p class="contact-name">Lorem ipsum</p>
+          <p class="last-message">Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div class="right-side">
+          <span class="pin">1</span>
+          <p class="last-message-date">Hier</p>
+        </div>
       </li>
     </ul>
   </div>

7d5e0a26c69afbcbb162fc9ec1fcaed7fa337c4f
MaDatNassu <madatman972@gmail.com>
2018-11-11 18:00:52 +0100

    adding clarity

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,18 @@
 <!doctype html>
 <html lang="fr">
     @include('partials.head')
+
     <body>
+
       @include('partials.header')
+
       <div class="main">
+
         @yield('content')
+
       </div>
+
     </body>
+
     @include('partials.foot')
 </html>

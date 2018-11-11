b655f7c0f5598739d48f6a7e212609c2ec0dee4a
MaDatNassu <madatman972@gmail.com>
2018-11-11 17:53:01 +0100

    front updated

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,47 +3,25 @@
 @section('content')
   <div class="threads">
     <ul class="unstyled-list">
-      <li class="thread">
-        <figure class="contact-img">
-          <img src="https://dummyimage.com/50x50/00aeff/000" alt="Contact">
-        </figure>
-        <div class="middle-side">
-          <p class="contact-name">Lorem ipsum</p>
-          <p class="last-message">Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div class="right-side">
-          <span class="pin">14</span>
-          <p class="last-message-date">00:14</p>
-        </div>
-      </li>
 
-      <li class="thread">
-        <figure class="contact-img">
-          <img src="https://dummyimage.com/50x50/00aeff/000" alt="Contact">
-        </figure>
-        <div class="middle-side">
-          <p class="contact-name">Lorem ipsum</p>
-          <p class="last-message">Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div class="right-side">
-          <span class="pin">2</span>
-          <p class="last-message-date">22:57</p>
-        </div>
-      </li>
+    @foreach ($threads as $thread)
+      @if ($thread->lastmessage != null)
+        <li class="thread">
+          <figure class="contact-img">
+            <img src="https://dummyimage.com/50x50/00aeff/000" alt="Contact">
+          </figure>
+          <div class="middle-side">
+            <p class="contact-name">Lorem ipsum</p>
+            <p class="last-message">{{ str_limit( $thread->lastmessage->message, 61, '...' ) }}</p>
+          </div>
+          <div class="right-side">
+            <span class="pin">{{ rand(0, 99) }}</span>
+            <p class="last-message-date">00:14</p>
+          </div>
+        </li>
+      @endif
+    @endforeach
 
-      <li class="thread">
-        <figure class="contact-img">
-          <img src="https://dummyimage.com/50x50/00aeff/000" alt="Contact">
-        </figure>
-        <div class="middle-side">
-          <p class="contact-name">Lorem ipsum</p>
-          <p class="last-message">Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div class="right-side">
-          <span class="pin">1</span>
-          <p class="last-message-date">Hier</p>
-        </div>
-      </li>
     </ul>
   </div>
 @endsection
